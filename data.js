0de089196358fbc2c55169f624f721d2455f9354
Rishik Gupta <rishik.gupta70@gmail.com>
2024-04-11 19:29:01 -0400

    Second commit

---
+++ data.js
@@ -0,0 +1,56 @@
+const userData = [
+    {
+     username: "Rishik",
+     password: "hello123",
+     userType: "student",
+     income: 100,
+     expense: 50,
+     balance: 50,
+     creationDate: "11\/04\/2024",
+     transactions : [
+        {
+            tNumber: 1,
+            tType: "income",
+            tCategory: "gift",
+            tDate: "11\/04\/2024",
+            tAmount: 100,
+            tDescription: "April Salary"
+        },
+        {
+            tNumber: 2,
+            tType: "expense",
+            tCategory: "outing",
+            tDate: "11\/04\/2024",
+            tAmount: 50,
+            tDescription: "Food & Drinks"
+        }
+     ]
+    },
+    {
+        username: "Dikunj",
+        password: "hello456",
+        userType: "Professional",
+        income: 1000,
+        expense: 500,
+        balance: 500,
+        creationDate: "2024\/04\/01",
+        transactions : [
+           {
+               tNumber: 1,
+               tType: "income",
+               tCategory: "salary",
+               tDate: "2024\/04\/01",
+               tAmount: 1000,
+               tDescription: "April Salary"
+           },
+           {
+            tNumber: 2,
+            tType: "expense",
+            tCategory: "transfer",
+            tDate: "2024\/04\/01",
+            tAmount: 500,
+            tDescription: "Transfer for loan Payment"
+        }
+        ]
+       }
+   ];
